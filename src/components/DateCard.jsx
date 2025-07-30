aae1b1f2908fe20771e9b377401342a8b26f77c4
juyoung <juyoungjeong.797@gmail.com>
2025-07-30 17:34:44 +0900

    idx

--- src/components/DateCard.jsx
+++ src/components/DateCard.jsx
@@ -1,12 +1,12 @@
-const DateCard = ({ date, todos, isSat, isHoliday }) => {
+const DateCard = ({ date, todos }) => {
   return (
     <div className="bg-white w-40 h-60 rounded-2xl">
       <div className="bg-red-400 text-2xl font-semibold text-right px-2 rounded-t-2xl">{date}</div>
       <div className="text-lg p-2"></div>
       <div className="text-lg p-2 flex-col gap-2">
-        <p>{todos.map((value) => (
-          <p>{value}</p>
-        ))}</p>
+        <div>{todos.map((value, idx) => (
+          <p key={idx}>{value}</p>
+        ))}</div>
       </div>
     </div>
   );
